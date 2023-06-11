5d360d60ec929ac4ed6f3d47cd6b6a9f6cdfca19
diogocardoso777 <77904256+diogocardoso777@users.noreply.github.com>
2023-06-11 21:36:50 +0100

    Fix settings for integrating React
    - migrations added

--- sitediam/sports24h/serializers.py
+++ sitediam/sports24h/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Post, Seller, Forum, Comment
 
@@ -7,11 +8,17 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ('pk', 'owner', 'title', 'forum', 'text', 'likes_count', 'created_at')
 
-#
-# class SellerSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Seller
-#         fields = ('pk', 'questao', 'opcao_texto', 'votos')
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('pk', 'username')
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seller
+        fields = ('pk', 'user')
 
 
 class CommentSerializer(serializers.ModelSerializer):
